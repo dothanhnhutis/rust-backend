2b328fa35025a1dea6552d7528bdbc6340bf263c
dothanhnhutis <dothanhnhutis@gmail.com>
2026-04-18 17:00:03 +0700

    first commit

---
+++ apps/api/src/main.rs
@@ -0,0 +1,26 @@
+use axum::{routing::get, Router};
+use infrastructure::init_db_pool;
+use dotenvy::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    
+    // 1. Khởi tạo Log
+    tracing_subscriber::fmt::init();
+
+    // 2. Kết nối Database từ Lib Infrastructure
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = init_db_pool(&db_url).await.expect("Failed to connect to DB");
+
+    // 3. Build route
+    let app = Router::new()
+        .route("/", get(|| async { "Hệ thống quản lý kho hóa chất sẵn sàng!" }))
+        .with_state(pool); // Chia sẻ pool cho các handler
+
+    // 4. Chạy server
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    tracing::info!("Server đang chạy tại: http://localhost:8080");
+    axum::serve(listener, app).await.unwrap();
+}
---
+++ libs/infrastructure/src/lib.rs
@@ -0,0 +1,12 @@
+use sqlx::postgres::{PgPoolOptions, PgPool};
+use std::time::Duration;
+
+pub async fn init_db_pool(database_url: &str) -> anyhow::Result<PgPool> {
+    let pool = PgPoolOptions::new()
+        .max_connections(20) // Phù hợp VPS 4GB
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(database_url)
+        .await?;
+    
+    Ok(pool)
+}
